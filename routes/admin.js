5624f54b25623d8f27c62936018263671d51acf1
Kusumkoli <kusumkoli372@gmail.com>
2020-04-03 21:50:41 -1200

    refactoring code

--- routes/admin.js
+++ routes/admin.js
@@ -11,4 +11,10 @@ router.get( '/add-product', productsController.getAddProduct);
 //    /admin/product
 router.post( '/add-product', productsController.postAddProduct);
 
+router.get('/products', productsController.getPro);
+
+router.get('/cart', );
+
+router.get('/checkout')
+
 module.exports = router;
